5e4219e43162c6026279771c5ecc0f52b35c1517
Jérôme Lebel <jerome@fotonauts.com>
2011-09-03 00:38:39 +0200

    adding support for ipv6

--- src/net.c
+++ src/net.c
@@ -54,10 +54,10 @@ int mongo_set_socket_op_timeout( mongo *conn, int millis ) {
     return MONGO_OK;
 }
 
-static int mongo_create_socket( mongo *conn ) {
+static int mongo_create_socket( mongo *conn, int domain, int type, int protocol ) {
     int fd;
 
-    if( ( fd = socket( AF_INET, SOCK_STREAM, 0 ) ) == -1 ) {
+    if( ( fd = socket( domain, type, protocol ) ) == -1 ) {
         conn->err = MONGO_CONN_NO_SOCKET;
         return MONGO_ERROR;
     }
@@ -67,27 +67,39 @@ static int mongo_create_socket( mongo *conn ) {
 }
 
 int mongo_socket_connect( mongo *conn, const char *host, int port ) {
-    struct sockaddr_in sa;
-    socklen_t addressSize;
+    struct addrinfo req, *ans;
+    int code;
     int flag = 1;
+    char serviceName[256];
 
-    if( mongo_create_socket( conn ) != MONGO_OK )
+    snprintf(serviceName, sizeof(serviceName), "%d", port);
+    req.ai_flags = 0;
+    req.ai_family = AF_UNSPEC;
+    req.ai_socktype = SOCK_STREAM;
+    req.ai_protocol = IPPROTO_TCP;
+    
+    req.ai_protocol = 0;
+    if ((code = getaddrinfo(host, serviceName, &req, &ans)) != 0) {
+        conn->err = MONGO_CONN_ADDR_FAIL;
+        freeaddrinfo(ans);
         return MONGO_ERROR;
+    }
+    
+    if( mongo_create_socket( conn, ans->ai_family, ans->ai_socktype, ans->ai_protocol ) != MONGO_OK ) {
+        freeaddrinfo(ans);
+        return MONGO_ERROR;
+    }
 
-    memset( sa.sin_zero , 0 , sizeof( sa.sin_zero ) );
-    sa.sin_family = AF_INET;
-    sa.sin_port = htons( port );
-    sa.sin_addr.s_addr = inet_addr( host );
-    addressSize = sizeof( sa );
-
-    if ( connect( conn->sock, ( struct sockaddr * )&sa, addressSize ) == -1 ) {
+    if ( connect( conn->sock, ans->ai_addr, ans->ai_addrlen ) == -1 ) {
         mongo_close_socket( conn->sock );
+        freeaddrinfo(ans);
         conn->connected = 0;
         conn->sock = 0;
         conn->err = MONGO_CONN_FAIL;
         return MONGO_ERROR;
     }
 
+    freeaddrinfo(ans);
     setsockopt( conn->sock, IPPROTO_TCP, TCP_NODELAY, ( char * ) &flag, sizeof( flag ) );
     if( conn->op_timeout_ms > 0 )
         mongo_set_socket_op_timeout( conn, conn->op_timeout_ms );
