31cac7b8a06671854e7d962a4f5d01340b11f5eb
Jérôme Lebel <jerome@fotonauts.com>
2011-09-12 21:06:12 +0200

    less warnings

--- src/mongo.c
+++ src/mongo.c
@@ -33,7 +33,7 @@
 
 static const int ZERO = 0;
 static const int ONE = 1;
-mongo_message *mongo_message_create( int len , int id , int responseTo , int op ) {
+static mongo_message *mongo_message_create( int len , int id , int responseTo , int op ) {
     mongo_message *mm = ( mongo_message * )bson_malloc( len );
 
     if ( !id )
@@ -49,7 +49,7 @@ mongo_message *mongo_message_create( int len , int id , int responseTo , int op 
 }
 
 /* Always calls bson_free(mm) */
-int mongo_message_send( mongo *conn, mongo_message *mm ) {
+static int mongo_message_send( mongo *conn, mongo_message *mm ) {
     mongo_header head; /* little endian */
     int res;
     bson_little_endian32( &head.len, &mm->head.len );
@@ -73,7 +73,7 @@ int mongo_message_send( mongo *conn, mongo_message *mm ) {
     return MONGO_OK;
 }
 
-int mongo_read_response( mongo *conn, mongo_reply **reply ) {
+static int mongo_read_response( mongo *conn, mongo_reply **reply ) {
     mongo_header head; /* header from network */
     mongo_reply_fields fields; /* header from network */
     mongo_reply *out;  /* native endian */
@@ -112,17 +112,17 @@ int mongo_read_response( mongo *conn, mongo_reply **reply ) {
 }
 
 
-char *mongo_data_append( char *start , const void *data , int len ) {
+static char *mongo_data_append( char *start , const void *data , int len ) {
     memcpy( start , data , len );
     return start + len;
 }
 
-char *mongo_data_append32( char *start , const void *data ) {
+static char *mongo_data_append32( char *start , const void *data ) {
     bson_little_endian32( start , data );
     return start + 4;
 }
 
-char *mongo_data_append64( char *start , const void *data ) {
+static char *mongo_data_append64( char *start , const void *data ) {
     bson_little_endian64( start , data );
     return start + 8;
 }
