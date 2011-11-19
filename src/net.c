4b997ae8a60d7920e3c96e7e4f1c13f912f3a4bf
Jérôme Lebel <jerome@fotonauts.com>
2011-11-19 12:18:55 +0100

    better support for ipv6 and ipv4

--- src/net.c
+++ src/net.c
@@ -18,6 +18,7 @@
 /* Implementation for generic version of net.h */
 #include "net.h"
 #include <string.h>
+#include <errno.h>
 
 int mongo_write_socket( mongo *conn, const void *buf, size_t len ) {
     const char *cbuf = buf;
@@ -66,9 +67,48 @@ static int mongo_create_socket( mongo *conn, int domain, int type, int protocol 
     return MONGO_OK;
 }
 
+static void print_ip_from_address( struct addrinfo *ans )
+{
+    char *proto_arr[] = { "IPv4","IPv6","UNIX" };
+    char *service=NULL;
+    int err;
+    struct sockaddr *sa;
+    int size;
+    int proto;
+    int nameinfoflags = NI_NUMERICHOST;
+    
+    char hostbuf[NI_MAXHOST];
+    char servbuf[NI_MAXSERV];
+    
+    
+    sa=ans->ai_addr;
+    size=0;
+    switch ( sa->sa_family ) {
+        case PF_INET:
+            size=sizeof( struct sockaddr_in );
+            proto=0;
+            break;
+        case PF_INET6:
+            size=sizeof( struct sockaddr_in6 );
+            proto=1;
+            break;
+        default:
+            fprintf( stderr, "Unknown protocol family in result: %d\n", sa->sa_family );
+    }
+    if ( size ) {
+        if ( (err = getnameinfo( sa, size, hostbuf, sizeof(hostbuf), servbuf, sizeof(servbuf), nameinfoflags ) ) ) {
+            fprintf( stderr,"%s\n", gai_strerror( err ) );
+        } else {
+            if ( service )
+                printf( "%s %s %s\n", proto_arr[proto], hostbuf, servbuf );
+            else
+                printf( "%s %s\n", proto_arr[proto], hostbuf );
+        }
+    }
+}
+
 int mongo_socket_connect( mongo *conn, const char *host, int port ) {
-    struct addrinfo req, *ans;
-    int code;
+    struct addrinfo req, *ans, *ans_cursor;
     int flag = 1;
     char serviceName[256];
 
@@ -79,33 +119,44 @@ int mongo_socket_connect( mongo *conn, const char *host, int port ) {
     req.ai_protocol = IPPROTO_TCP;
     
     req.ai_protocol = 0;
-    if ( ( code = getaddrinfo( host, serviceName, &req, &ans ) ) != 0 ) {
+    if ( getaddrinfo( host, serviceName, &req, &ans ) != 0 ) {
+        printf( "cannot get address info, error %d\n", errno );
+        perror( "getaddrinfo error" );
         conn->err = MONGO_CONN_ADDR_FAIL;
         freeaddrinfo( ans );
         return MONGO_ERROR;
     }
     
-    if( mongo_create_socket( conn, ans->ai_family, ans->ai_socktype, ans->ai_protocol ) != MONGO_OK ) {
-        freeaddrinfo( ans );
-        return MONGO_ERROR;
+    // test all ips that match the host (this is useful when entering localhost, since it will match 127.0.0.1 and ::1
+    // but the mongo server might listen only to 127.0.0.1 or ::1, so we need to try all ips).
+    ans_cursor = ans;
+    while ( ans_cursor ) {
+        if( mongo_create_socket( conn, ans_cursor->ai_family, ans_cursor->ai_socktype, ans_cursor->ai_protocol ) != MONGO_OK ) {
+            printf( "cannot create socket, error %d\n", errno );
+            perror( "mongo_create_socket error" );
+        } else if ( connect( conn->sock, ans_cursor->ai_addr, ans_cursor->ai_addrlen ) == -1 ) {
+            print_ip_from_address( ans_cursor );
+            printf( "can not connect to %s:%d with socket %d, error number %d \n", host, port, conn->sock, errno );
+            perror( "connect error" );
+            mongo_close_socket( conn->sock );
+        } else {
+            break;
+        }
+        ans_cursor = ans_cursor->ai_next;
     }
 
-    if ( connect( conn->sock, ans->ai_addr, ans->ai_addrlen ) == -1 ) {
-        printf( "can not connect to %s:%d with socket %d\n", host, port, conn->sock );
-        mongo_close_socket( conn->sock );
-        freeaddrinfo( ans );
+    freeaddrinfo( ans );
+    if ( ans_cursor ) {
+        setsockopt( conn->sock, IPPROTO_TCP, TCP_NODELAY, ( char * ) &flag, sizeof( flag ) );
+        if( conn->op_timeout_ms > 0 )
+            mongo_set_socket_op_timeout( conn, conn->op_timeout_ms );
+
+        conn->connected = 1;
+        return MONGO_OK;
+    } else {
         conn->connected = 0;
         conn->sock = 0;
         conn->err = MONGO_CONN_FAIL;
         return MONGO_ERROR;
     }
-
-    freeaddrinfo( ans );
-    setsockopt( conn->sock, IPPROTO_TCP, TCP_NODELAY, ( char * ) &flag, sizeof( flag ) );
-    if( conn->op_timeout_ms > 0 )
-        mongo_set_socket_op_timeout( conn, conn->op_timeout_ms );
-
-    conn->connected = 1;
-
-    return MONGO_OK;
 }
