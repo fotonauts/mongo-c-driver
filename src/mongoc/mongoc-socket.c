f10b94f1865ce64e124ed08b8dcbb9da76aebdd8
Jérôme Lebel <jerome@fotonauts.com>
2014-10-06 19:47:48 +0200

    no real need to show explicitly ipv4/ipv6

--- src/mongoc/mongoc-socket.c
+++ src/mongoc/mongoc-socket.c
@@ -1102,13 +1102,13 @@ mongoc_socket_inet_ntop (struct addrinfo    *rp,     /* IN */
     switch (rp->ai_family) {
         case AF_INET:
             ptr = &((struct sockaddr_in *) rp->ai_addr)->sin_addr;
-            inet_ntop (rp->ai_family, ptr, tmp, sizeof(tmp));
-            bson_snprintf (buf, buflen, "ipv4 %s", tmp);
+            inet_ntop (rp->ai_family, ptr, tmp, sizeof tmp);
+            bson_snprintf (buf, buflen, "%s", tmp);
             break;
         case AF_INET6:
             ptr = &((struct sockaddr_in6 *) rp->ai_addr)->sin6_addr;
-            inet_ntop (rp->ai_family, ptr, tmp, sizeof(tmp));
-            bson_snprintf (buf, buflen, "ipv6 %s", tmp);
+            inet_ntop (rp->ai_family, ptr, tmp, sizeof tmp);
+            bson_snprintf (buf, buflen, "s%s", tmp);
             break;
         default:
             bson_snprintf (buf, buflen, "unknown ip %d", rp->ai_family);
